147b5d6e7a1a3c06939ac6c77988395147596c2d
Kojiiwasaki <cest-lavie@outlook.jp>
2020-03-19 16:38:04 +0900

    hello1.java更新

--- hello1.java
+++ hello1.java
@@ -1,5 +1,5 @@
-public class Hello1{
-    public static void main(string args[]){
+public class hello1{
+    public static void main(String[] args){
         System.out.println("こんにちはJava");
     }
 }
